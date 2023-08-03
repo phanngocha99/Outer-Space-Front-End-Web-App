47c6541738b8b08335960f099e4146076ff63beb
Phan Ngọc Lunah Hà <ngochaphan.work@gmail.com>
2023-08-03 17:23:51 +0700

    update img

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,11 +1,13 @@
 import '../style/banner.css'
+import banner1 from '../img/banner-1.png'
+import banner2 from '../img/banner-2.png'
 
 export default function Banner() {
     return (
         <div id="banner-home" className="banner">
             <div className='banner-img'>
-                <img className="img-banner-1st" src="./img/banner-1.png" alt="banner" />
-                <img className="img-banner-2nd" src="./img/banner-2.png" alt="banner" />
+                <img className="img-banner-1st" src={banner1} alt="banner" />
+                <img className="img-banner-2nd" src={banner2} alt="banner" />
             </div>
             <div className="banner-text">
                 <i>
