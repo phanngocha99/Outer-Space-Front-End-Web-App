ba49a1d2159abeb4ac0b6197946df0e882592ae4
Phan Ngọc Lunah Hà <ngochaphan.work@gmail.com>
2023-08-03 16:54:33 +0700

    update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,7 +4,7 @@ export default function Banner() {
     return (
         <div id="banner-home" className="banner">
             <div className='banner-img'>
-                <img className="img-banner-1st" src="../public/img/banner-1.png" alt="banner" />
+                <img className="img-banner-1st" src="./img/banner-1.png" alt="banner" />
                 <img className="img-banner-2nd" src="./img/banner-2.png" alt="banner" />
             </div>
             <div className="banner-text">
